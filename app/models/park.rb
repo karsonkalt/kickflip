9944a4b09e37acce5e10eb906f9169817c4ca80e
Karson <kars.kalt@gmail.com>
2021-02-07 14:09:01 -0500

    Park and User find_top methods reformatted code, returns objects not name of objects

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,18 +1,21 @@
 class Park < ActiveRecord::Base
+    #Geocoder Gem
     extend Geocoder::Model::ActiveRecord
 
     geocoded_by :address
-    after_validation :geocode
-    #check on what exactly line 5 is doing.
+    after_validation :geocode #check on what exactly this line is doing
 
+    #Relationships
     has_many :skate_sessions
     has_many :users, through: :skate_sessions
 
+    #Validations
     validates :name, presence: true, uniqueness: true
     validates :street, presence: true
     validates :city, presence: true
     validates :state, presence: true
 
+    #Methods for Geocoder
     def address
         [self.street, self.city, self.state].compact.join(', ')
     end
@@ -22,15 +25,27 @@ class Park < ActiveRecord::Base
         self.geocode
     end
 
+    #Custom Methods
     def top_users
         counts = Hash.new(0) #creates a new hash and sets default value to 0, {} default value is nil
         self.users.each do |user|
             counts[user.username] += 1
         end
-        counts.sort_by {|username, count| -count} #returns an array sorted most skate sessions logged, to least skate sessions logged
+        counts_array = counts.map do |username, count|
+            [User.find_by(username: username), count]
+        end
+
+        counts_array.sort_by {|username, count| -count} #returns an array sorted most skated parks, to least skated parks logged
+        #Returns array of arrays sorted top to bottom
     end
 
     def top_x_users(x)
-        self.top_users[0..(x-1)]
+        if x == 1
+            self.top_users[0][0] #Returns single oject
+        elsif x > 1
+            self.top_users[0..(x-1)] #Returns array of arrays
+        else
+            nil
+        end
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,37 @@
 class User < ActiveRecord::Base
+    #Relationships
     has_many :skate_sessions
     has_many :parks, through: :skate_sessions
 
-    # validates :username, presence: true, uniqueness: true
+    #Validations
+    validates :username, presence: true, uniqueness: true
     # validates :password, presence: true
 
     # has_secure_password
+
+    #Custom Methods
+    def top_parks
+        counts = Hash.new(0) #Creates a new hash and sets default value to 0, {} default value is nil
+        
+        self.parks.each do |park|
+            counts[park.name] += 1
+        end
+        
+        counts_array = counts.map do |name, count|
+            [Park.find_by(name: name), count]
+        end
+
+        counts_array.sort_by {|name, count| -count} #Returns array of arrays sorted by Park with the most SkateSessions to least
+    end
+
+    def top_x_parks(x)
+        if x == 1
+            self.top_parks[0][0] #Returns single oject
+        elsif x > 1
+            self.top_parks[0..(x-1)] #Returns array of arrays
+        else
+            nil
+        end
+    end
 
 end
