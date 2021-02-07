0b65921e4d839ca07e175356d11b81c856294542
Karson <kars.kalt@gmail.com>
2021-02-07 12:10:28 -0500

    Adding migrations for new tables.

--- app/models/park.rb
+++ app/models/park.rb
@@ -3,7 +3,7 @@ class Park < ActiveRecord::Base
 
     geocoded_by :address
     after_validation :geocode
-    #check on what exactly line 6 is doing.
+    #check on what exactly line 5 is doing.
 
     has_many :skate_sessions
     has_many :users, through: :skate_sessions
