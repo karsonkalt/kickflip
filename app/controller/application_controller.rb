2f1a65b8af74e666cb76b2adab272b3a1eaa1ecb
Karson <kars.kalt@gmail.com>
2021-02-10 11:29:27 -0500

    Added admin star, updated login to validate user. It was crashing before.

--- app/controller/application_controller.rb
+++ app/controller/application_controller.rb
@@ -52,8 +52,12 @@ class ApplicationController < Sinatra::Base
 
     post '/login' do
         user = User.find_by(email: params["email"])
-        session[:user_id] = user.id
-        redirect "/users/#{user.id}"
+        if user != nil
+            session[:user_id] = user.id
+            redirect "/users/#{user.id}"
+        else
+            redirect "login"
+        end
     end
 
     get '/logout' do
